ef05225a553247d5197952acf9dfd52ee51c5fc9
luveshanm <u17087709@tuks.co.za>
2019-05-03 00:01:41 +0200

    testing stuff

--- FABI-Web/src/karma.conf.js
+++ FABI-Web/src/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
+    mime: { 'text/x-typescript': ['ts','tsx'] },
     restartOnFileChange: true
   });
 };
